250737235e963e0ac5675e3c59f822da4d7bdc89
LogeshwaranRAJAGOPAL <100465267+LogeshwaranRAJAGOPAL@users.noreply.github.com>
2023-02-13 08:50:24 +0530

    Update UpdatePlayer.js

--- frontend/src/components/js/UpdatePlayer.js
+++ frontend/src/components/js/UpdatePlayer.js
@@ -57,46 +57,46 @@ const UpdatePlayer = (props)=>{
 
     const handleChange1 =  (element)=>{
         setFirstName(element.target.value);
-        setState(!state);
+        setState(true);
     }
     const handleChange2 =  (element)=>{
         setLastName(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange3 =  (element)=>{
         setCommonName(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange4 =  (element)=>{
         setAge(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange5 =  (element)=>{
         setSpeed(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange6 =  (element)=>{
         setSkills(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange7 =  (element)=>{
         setAgility(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange8 =  (element)=>{
         setPosition(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
     const handleChange9 =  (element)=>{
         setImageOfplayer(element.target.value);
-        setState(!state);
+        setState(true);
     }
 
 
